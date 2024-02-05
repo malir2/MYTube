b51666b4c980092c0dd8b81dac2f4c5124781644
malir2 <maliramzan314@gmail.com>
2024-02-05 20:04:49 +0500

    User router updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import {
+  changePassword,
+  changeProfileImage,
+  getChannelsDetails,
+  getUser,
   logOutUser,
   loginUser,
   refreshAccessToken,
+  updateCoverImage,
   userRegister,
+  watchHistory,
 } from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,6 +29,25 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logOutUser);
+
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changePassword);
+
+router.route("/change-profile-info").post(verifyJWT, changeProfileImage);
+
+router
+  .route("/change-profile-image")
+  .patch(verifyJWT, upload.single("avatar"), changeProfileImage);
+
+router
+  .route("/change-profile-coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/user-info").get(getUser);
+
+router.route("/c:/username").get(verifyJWT, getChannelsDetails);
+
+router.route("/watch-history").get(verifyJWT, watchHistory);
 
 export default router;
